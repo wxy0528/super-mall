da9bdbbfac5216176a029ca34afb3e6d97c00808
文思宇 <1277183820@qq.com>
2020-05-03 08:40:52 +0800

    ok

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+export const configureWebpack = {
+  resolve: {
+    //   别名配置
+    alias:{
+        'assets':'@/assets',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+    }
+  },
+};
