5df5f345fb1fb5055d6b8d199992a80f7ac1269a
文思宇 <1277183820@qq.com>
2020-06-08 08:25:10 +0800

    goods

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="goodlist">
+      <ul>
+          <li>1级标题</li>
+          <li>2级标题</li>
+          <li>3级标题</li>
+          <li>4级标题</li>
+          <li>5级标题</li>
+          <li>6级标题</li>
+          <li>7级标题</li>
+          <li>8级标题</li>
+          <li>9级标题</li>
+          <li>10级标题</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+
+</style>
